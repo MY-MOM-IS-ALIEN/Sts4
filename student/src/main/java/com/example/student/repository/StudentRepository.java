09d71d91c72d53910a939813c1ee266f7b90349f
MY-MON-IS-ALIEN <alsdn5471@naver.com>
2024-01-15 15:01:29 +0900

    학생조회까지

---
+++ student/src/main/java/com/example/student/repository/StudentRepository.java
@@ -0,0 +1,30 @@
+package com.example.student.repository;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.example.student.dto.StudentDTO;
+
+@Repository
+public class StudentRepository {
+	@Autowired
+	private SqlSessionTemplate sql;
+	
+
+	public void save(StudentDTO studentDTO) {
+		sql.insert("Student.save",studentDTO);
+		// 1번 매개변수 = Student =  <mapper namespace="Student">
+		// 1번 매개변수 = save = <insert id="save"
+		// 2번 매개변수 = 넘기는 데이터의 타입  parameterType="com.example.Student.dto.StudentDTO">
+		// 풀 패키지 경로
+		
+	}
+
+
+	public List<StudentDTO> findAll() {
+		return sql.selectList("Student.findAll"); 
+	}
+}
